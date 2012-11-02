6ee87ed4af9e6740b9f40e9d60d9c2de5cd1c290
admin <admin@ST-02>
2012-11-02 10:20:07 +0900

    버튼

---
+++ src/com/example/test_1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.test_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	
+	
+
+	Button btn1;
+	TextView textview1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+        
+		
+        btn1 = (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
+        textview1 = (TextView)findViewById(R.id.textView1);
+    }
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		textview1.setText("Android");
+		
+		
+		
+	}
+}
